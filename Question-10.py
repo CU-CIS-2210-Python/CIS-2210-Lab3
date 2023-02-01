670fbc75085e48cd1ecfc41129c9e5b169a379e2
Test Student <cuteststudent@billkuker.com>
2023-02-01 13:14:16 -0500

    Lab 3 code

---
+++ Question-10.py
@@ -0,0 +1,9 @@
+print("Welcome to the Temperature Converter program.")
+
+c = float(input("Enter a temperatire in °C: "))
+f = (c * (9 / 5)) + 32
+print(str(c) + " is equal to " + str(f) + "°F")
+
+c = float(input("Enter a temperatire in °C: "))
+f = (c * (9 / 5)) + 32
+print(str(c) + " is equal to " + str(f) + "°F")
